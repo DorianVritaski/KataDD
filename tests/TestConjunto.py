89a4fa927b134970cd9c8e49a6e2dc2654d399ca
Jhann Pier Tapia De La Cruz <72947488@continental.edu.pe>
2024-10-15 16:32:15 -0500

    caso 1:Calcular promedio de un conjunto de dos elementos vacio rojo

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -4,4 +4,12 @@ from src.logica.Conjunto import Conjunto
 class TestConjunto( unittest.TestCase ):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
-        self.assertIsNone()
+        self.assertIsNone(conjunto.promedio())
+
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
+
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7])
+        self.assertEqual(6, conjunto.promedio())
